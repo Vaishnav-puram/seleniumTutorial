bd7627b0a0a186e23b2ffc7e7da3ee234403f0ac
Guest007 <puramv307@gmail.com>
2023-07-22 08:45:20 +0530

    demo for executing test methods parallelly

--- src/main/java/org/example/TestNg/ListenersDemo.java
+++ src/main/java/org/example/TestNg/ListenersDemo.java
@@ -31,4 +31,42 @@ public class ListenersDemo {
         }
         driver.quit();
     }
+    @Test
+    @Parameters({"A"})
+    public void test2(int a){
+        int b=5,c=6;
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "src/main/java/driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://omayo.blogspot.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        if(a==1){
+            Assert.assertTrue(b<c);
+        } else if (a==2) {
+            Assert.assertTrue(b>c);
+        } else if (a==3) {
+            throw new SkipException("Test is skipped");
+        }
+        driver.quit();
+    }
+    @Test
+    @Parameters({"A"})
+    public void test3(int a){
+        int b=5,c=6;
+        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "src/main/java/driver/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://omayo.blogspot.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        if(a==1){
+            Assert.assertTrue(b<c);
+        } else if (a==2) {
+            Assert.assertTrue(b>c);
+        } else if (a==3) {
+            throw new SkipException("Test is skipped");
+        }
+        driver.quit();
+    }
 }
